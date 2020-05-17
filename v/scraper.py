0afe41d831edcac00cf9748fd9796d339a92aa65
ariffrahimin <57780003+ariffrahimin@users.noreply.github.com>
2020-05-17 22:57:19 +0800

    First commit

--- v/scraper.py
+++ v/scraper.py
@@ -1,5 +1,5 @@
 import requests, sys, time, os, argparse
-
+#This is a Subterra code, modified by Ariff Rahimin
 # List of simple to collect features
 snippet_features = ["title",
                     "publishedAt",
